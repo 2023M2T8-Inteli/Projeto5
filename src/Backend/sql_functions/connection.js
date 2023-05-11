a970d1737fa3ce3fcbb616204271aa6cd53ed914
Maurício Azevedo <mauricio.neto@sou.inteli.edu.br>
2023-05-11 15:52:42 -0300

    feat: adiciona arquivo base para conexão ao banco de dados

---
+++ src/Backend/sql_functions/connection.js
@@ -0,0 +1,17 @@
+const sqlite3 = require("sqlite3").verbose();
+const DBPATH = "./database/database.db";
+
+function execSqlQquery(query, res) {
+  var db = new sqlite3.Database(DBPATH);
+  db.all(query, [], (error, rows) => {
+    if (error) {
+      throw error;
+    } else {
+      res.json(rows);
+    }
+  });
+
+  db.close();
+}
+
+module.exports = execSqlQquery;
