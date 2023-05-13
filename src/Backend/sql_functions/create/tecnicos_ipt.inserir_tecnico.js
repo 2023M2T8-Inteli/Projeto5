e25273bcd5b6000fb1f3a5776bacf3262a8ca1c0
Maurício Azevedo <mauricio.neto@sou.inteli.edu.br>
2023-05-13 13:53:11 -0300

    style: altera nome da função de inserirEmpresa para inserirTecnico

--- src/Backend/sql_functions/create/tecnicos_ipt.inserir_tecnico.js
+++ src/Backend/sql_functions/create/tecnicos_ipt.inserir_tecnico.js
@@ -1,9 +1,9 @@
 const execSqlQquery = require("../connection");
 
-function inserirEmpresa(user_tecnico, senha_tecnico, nome_tecnico, res) {
+function inserirTecnico(user_tecnico, senha_tecnico, nome_tecnico, res) {
   const query = `INSERT INTO tecnicos_ipt (user_tecnico, senha_tecnico, nome_tecnico) VALUES ('${user_tecnico}', '${senha_tecnico}', '${nome_tecnico}')`;
 
   execSqlQquery(query, res);
 }
 
-module.exports = inserirEmpresa;
+module.exports = inserirTecnico;
