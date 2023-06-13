e5d169e6d716ce96f921de4d4b5d8e37b51d0052
Maurício Azevedo <mauricio.neto@sou.inteli.edu.br>
2023-06-13 14:46:29 -0300

    docs: adiciona comentários sobre a funcionalidade dos arquivos de rotas frontend

--- src/Backend/routes/frontendRoutes/get_comparacao.js
+++ src/Backend/routes/frontendRoutes/get_comparacao.js
@@ -1,7 +1,11 @@
+//Importa o express e insere na variável 'router' a classe 'Router' do express
 var express = require("express");
 var router = express.Router();
+
+//Importação do módulo path para facilitar o trabalho com caminhos dos arquivos
 const path = require("path");
 
+//Define uma rota do tipo get
 router.get("/", (_, res) => {
   res.sendFile(
     path.resolve(
@@ -10,4 +14,5 @@ router.get("/", (_, res) => {
   );
 });
 
+//Exportação do módulo para uso da rota no app.js
 module.exports = router;
--- src/Backend/routes/frontendRoutes/get_main.js
+++ src/Backend/routes/frontendRoutes/get_main.js
@@ -1,11 +1,16 @@
+//Importa o express e insere na variável 'router' a classe 'Router' do express
 var express = require("express");
 var router = express.Router();
+
+//Importação do módulo path para facilitar o trabalho com caminhos dos arquivos
 const path = require("path");
 
+//Define uma rota do tipo get
 router.get("/", (_, res) => {
   res.sendFile(
     path.resolve(__dirname + "./../../../frontend/paginas/login/login.html")
   );
 });
 
+//Exportação do módulo para uso da rota no app.js
 module.exports = router;
